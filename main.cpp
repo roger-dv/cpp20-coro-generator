0b990f8aea39bcb4d28cd33e9783773bdae4ef9c
Roger Voss <Roger.Voss@tideworks.com>
2020-10-11 00:00:46 -0700

    now uses a variadic templated print function to show results

--- main.cpp
+++ main.cpp
@@ -33,23 +33,33 @@ generator<double> fibonacci(const double ceiling) {
 }
 #pragma clang diagnostic pop
 
+template <class T>
+void print(T &&arg) {
+  std::cout << arg << ' ';
+}
+template <class T, class ... Ts>
+void print(T &&arg, Ts &&... args) {
+  print(std::forward<T>(arg));
+  print(std::forward<Ts>(args)...);
+}
+
 int main() {
   std::cout << "Example using C++20 coroutines to implement Simple Integer and Fibonacci Sequence generators" << '\n';
 
-  std::cout << '\n' << "Simple Integer Sequence Generator" << '\n';
+  std::cout << '\n' << "Simple Integer Sequence Generator" << '\n' << ' ';
   auto iter1 = f();
   for(int i = 1; i <= 10; i++) {
     if (iter1.next()) {
       const auto value = iter1.getValue();
-      std::cout << i << ": " << value << '\n';
+      print(i, ':', value, '\n');
     }
   }
 
-  std::cout << '\n' << "Fibonacci Sequence Generator" << '\n';
+  std::cout << '\n' << "Fibonacci Sequence Generator" << '\n' << ' ';
   int i = 1;
   auto iter2 = fibonacci(demo_ceiling);
   while(iter2.next()) {
     const auto value = iter2.getValue();
-    std::cout << i++ << ": " << value << '\n';
+    print(i++, ':', value, '\n');
   }
 }
